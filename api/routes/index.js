d72bebb42865b0795cdc71af421d29f39f3e630a
Dhananajaya <dhananjay.m1805@gmailcom>
2025-03-16 12:29:09 +0530

    inital setup with routes and db connection

---
+++ api/routes/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+// Health Check API
+router.get("/status", function (req, res, next) {
+  return res.json({ code: 200, message: "Server UP AND Running, OK!" });
+});
+
+module.exports = router;
